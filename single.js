85fe7579f343fc04623b6a288959c4fe73b84781
Tilde Sorensen <tiso0001@stud.kea.dk>
2025-01-31 10:31:22 +0100

    en masse rettelser

--- single.js
+++ single.js
@@ -6,16 +6,27 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
 
 function showProduct(data) {
   produkt_billedeContainer.innerHTML = `
-   
+   <div>
         <img
           src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp"
           alt="Billede af produkt"/>
-    
+    </div>
      <div>
         <h3>${data.productdisplayname}</h3>
         <p><strong>Kategori:</strong>${data.category}</p>
         <p><strong>Type:</strong> ${data.articletype}</p>
         <p><strong>Pris:</strong> DKK ${data.price},-</p>
       </div>
+      <aside>
+        <label for="size">Vælg størrelse</label>
+        <select id="size">
+          <option>XS</option>
+          <option>S</option>
+          <option>M</option>
+          <option>L</option>
+          <option>XL</option>
+        </select>
+        <button class="tilfoj_til_kurv">Tilføj til kurv</button>
+      </aside>
       `;
 }
