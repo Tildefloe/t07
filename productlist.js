4f71f779ce0d73c5a8c13fc56080375972d37009
Tilde Sorensen <tiso0001@stud.kea.dk>
2025-02-05 11:33:40 +0100

    indsætter rabat og udsolgt samt lidt css

--- productlist.js
+++ productlist.js
@@ -15,14 +15,23 @@ function showList(data) {
     .map(
       (product) => ` 
           
-      <article class="leggings">
+      <article class="product_container">
       <a href="product.html?id=${product.id}">
             <img
               src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
               alt="Billede af produkt"
             />
+             <p class="soldout_hidden ${
+               product.soldout && "soldout"
+             }">Sold Out</p>
+             <p class="discount_hidden ${
+               product.discount && "discount"
+             }">On Sale</p>
             <h3 class="productdisplayname">${product.productdisplayname}</h3>
             <h5>DKK ${product.price},-</h5>
+            <p class="discount_hidden ${product.discount && "discount"}"> -${
+        product.discount
+      }% </p>
             </a>
           </article>
           `
